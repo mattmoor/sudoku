3edf5567a48cae8449b30e1d832f81e13d5bbad2
Matt Moore <mattomata@gmail.com>
2021-04-10 07:55:46 -0700

    Create bitset, split tests.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
 mod game;
 
-const ANY: game::Cell = game::Cell::Options(game::ALL);
-
 fn main() {
-    let v = game::Cell::Value;
-
-    let mut input = game::Board {
-        cells: [
-            [ANY, ANY, v(5), v(3), ANY, ANY, ANY, ANY, ANY],
-            [v(8), ANY, ANY, ANY, ANY, ANY, ANY, v(2), ANY],
-            [ANY, v(7), ANY, ANY, v(1), ANY, v(5), ANY, ANY],
-            [v(4), ANY, ANY, ANY, ANY, v(5), v(3), ANY, ANY],
-            [ANY, v(1), ANY, ANY, v(7), ANY, ANY, ANY, v(6)],
-            [ANY, ANY, v(3), v(2), ANY, ANY, ANY, v(8), ANY],
-            [ANY, v(6), ANY, v(5), ANY, ANY, ANY, ANY, v(9)],
-            [ANY, ANY, v(4), ANY, ANY, ANY, ANY, v(3), ANY],
-            [ANY, ANY, ANY, ANY, ANY, v(9), v(7), ANY, ANY],
-        ],
-    };
+    let mut input = game::Board::new([
+        [0, 0, 5, 3, 0, 0, 0, 0, 0],
+        [8, 0, 0, 0, 0, 0, 0, 2, 0],
+        [0, 7, 0, 0, 1, 0, 5, 0, 0],
+        [4, 0, 0, 0, 0, 5, 3, 0, 0],
+        [0, 1, 0, 0, 7, 0, 0, 0, 6],
+        [0, 0, 3, 2, 0, 0, 0, 8, 0],
+        [0, 6, 0, 5, 0, 0, 0, 0, 9],
+        [0, 0, 4, 0, 0, 0, 0, 3, 0],
+        [0, 0, 0, 0, 0, 9, 7, 0, 0],
+    ])
+    .expect("building board literal");
     input.check().expect("Failed to validate board.");
     // Run the solver.
     input.solve().expect("error finding solution");
